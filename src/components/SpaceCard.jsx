d07b6bb106be419d82b50e0dbef38612dc83e580
Aryan <69583042+aryannxroot@users.noreply.github.com>
2024-07-16 19:35:07 +0530

    some alterations now responsive

--- src/components/SpaceCard.jsx
+++ src/components/SpaceCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import whiteAirplane from '../assets/images/black_airplane_doodle.png'
+import blackAirplane from '../assets/images/black_airplane_doodle.png'
 const SpaceCard = () => {
   return (
-    <div className='mx-2 px-6 py-4 flex flex-col items-center justify-center border-2 border-gray-200 rounded-xl cursor-pointer hover:bg-gray-50'>
-      <img src={whiteAirplane} className='w-[50px] h-[50px] object-cover mb-4'/>
-      <h1 className='text-[1.5rem] font-[400] text-black-400 opacity-60 my-2 text-center'>Teach me about partial derivatives</h1>
+    <div className='flex flex-row md:flex-col items-center p-2 m-2 border-2 border-gray-200 rounded-xl mb-2 hover:bg-gray-100 cursor-pointer'>
+      <img src={blackAirplane} className='w-[25px] h-auto md:w-[40px] lg:w-[50px] xl:w-[60px]  mr-2'/>
+      <h1 className='text-gray-600 text-center md:mb-2 md:text-xl xl:my-4 xl:mx-2 xl:text-[1.6rem]'>Teach me about partial derivatives</h1>
     </div>
   )
 }
--- src/components/SpaceCards.jsx
+++ src/components/SpaceCards.jsx
@@ -1,11 +1,10 @@
-import SpaceCard from './SpaceCard';
 import React from 'react'
-
+import SpaceCard from './SpaceCard'
 const SpaceCards = () => {
   return (
-    <div className='flex flex-row justify-between items-center mt-[4rem] '>
+    <div className='my-4 max-w-[60%] md:flex justify-center gap-3.5 md:flex-row lg:w-10/12 md:w-9/12'>
       <SpaceCard />
-      <SpaceCard /> 
+      <SpaceCard />
       <SpaceCard />
     </div>
   )
