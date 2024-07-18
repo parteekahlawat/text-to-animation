78933aa6437a6a8b80afb8d551506dd9ef9f55a2
Aryan <69583042+aryannxroot@users.noreply.github.com>
2024-07-18 14:04:56 +0530

    Added hero section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,12 +7,28 @@ import Section2 from '../components/Section2'
 import Section3 from '../components/Section3'
 import Testimonial from '../components/Testimonial'   
 import FAQs from '../components/FAQs'   
+
+import icon1 from '../assets/images/random1.webp'
+import icon2 from '../assets/images/random2.webp'
+import icon3 from '../assets/images/random3.webp'
+import icon4 from '../assets/images/random4.webp'
+import icon5 from '../assets/images/random5.webp'
+import icon6 from '../assets/images/random6.webp'
+
+
+
 const Homepage = () => {
   return (
     <>
       <div className='w-fit'>
         <div className='relative w-full h-[55vh] object-cover bg-gradient-to-b from-transparent to-white '>
           <img src={maskGrid} className=' w-full h-full object-cover absolute z-[-1]'/>
+          <img src={icon1} className='absolute top-[10%] right-[30%]' />
+          <img src={icon2} className='absolute top-[40%] left-[30%]'  />
+          <img src={icon3} className='absolute bottom-[20%] right-[10%]' />
+          <img src={icon4} className='absolute bottom-[10%] right-[80%]' />
+          <img src={icon5} className='absolute bottom-[45%] right-[45%]' />
+          <img src={icon6} className='absolute top-[25%] left-[15%]' />
           <HomeNavbar />
         </div>
           <Hero />
